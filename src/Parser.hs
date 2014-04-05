e242ce263e4cca5a19488b97be9b2bc0496cf8d5
Taiki ONO <taiks.4559@gmail.com>
2014-04-06 04:21:29 +0900

    Fix parser for new instructions

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,7 +4,7 @@ module Parser
 
 import Control.Applicative ((<$>), (<$))
 import Data.Maybe (catMaybes)
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec hiding (label)
 import VM.Instruction
 
 instructionsParser :: Parser [Instruction]
@@ -12,7 +12,20 @@ instructionsParser = catMaybes <$> (comment <|> instruction <|> blank) `sepBy` m
                 where blank = Nothing <$ eof
 
 instruction :: Parser (Maybe Instruction)
-instruction = Just <$> choice (try <$> [add, sub, store, read', push])
+instruction = Just <$> choice (try <$> [ add
+                                       , sub
+                                       , lt
+                                       , le
+                                       , gt
+                                       , ge
+                                       , eq
+                                       , not'
+                                       , store
+                                       , load
+                                       , push
+                                       , label
+                                       , jump
+                                       , jumpIf ])
 
 add :: Parser Instruction
 add = Add <$ string "Add"
@@ -20,17 +33,47 @@ add = Add <$ string "Add"
 sub :: Parser Instruction
 sub = Sub <$ string "Sub"
 
+lt :: Parser Instruction
+lt = Lt <$ string "Lt"
+
+le :: Parser Instruction
+le = Le <$ string "Le"
+
+gt :: Parser Instruction
+gt = Gt <$ string "Gt"
+
+ge :: Parser Instruction
+ge = Ge <$ string "Ge"
+
+eq :: Parser Instruction
+eq = Eq <$ string "Eq"
+
+not' :: Parser Instruction
+not' = Not <$ string "Not"
+
 store :: Parser Instruction
 store = Store <$> instWithNumber "Store"
 
-read' :: Parser Instruction
-read' = Load <$> instWithNumber "Load"
+load :: Parser Instruction
+load = Load <$> instWithNumber "Load"
 
 push :: Parser Instruction
 push = Push <$> instWithNumber "Push"
 
+label :: Parser Instruction
+label = Label <$> instWithString "Label"
+
+jump :: Parser Instruction
+jump = Jump <$> instWithString "Jump"
+
+jumpIf :: Parser Instruction
+jumpIf = JumpIf <$> instWithString "JumpIf"
+
 instWithNumber :: String ->  Parser Integer
 instWithNumber s = read <$> (string s >> spaces >> many1 digit)
+
+instWithString :: String -> Parser String
+instWithString s = string s >> spaces >> many (try $ noneOf "\n")
 
 comment :: Parser (Maybe Instruction)
 comment = char '#' >> many (noneOf "\n") >> return Nothing
