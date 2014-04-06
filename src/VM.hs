d8561a383a9388da3e6710e05f20539fff2cfa2f
Taiki ONO <taiks.4559@gmail.com>
2014-04-06 15:12:31 +0900

    Use Array to store instructions

--- src/VM.hs
+++ src/VM.hs
@@ -5,6 +5,7 @@ module VM
     , takePC ) where
 
 import Control.Applicative ((<$>))
+import Data.Array
 import Data.Monoid
 import Data.Foldable (foldMap)
 
@@ -12,14 +13,17 @@ import VM.Machine
 import VM.Instruction
 
 runVM :: [Instruction] -> Machine
-runVM is = run (instMorph <$> is) (setCounter 0 . prepare is $ initMachine)
+runVM is = run (toArray $ instMorph <$> is) (setCounter 0 . prepare is $ initMachine)
 
-run :: [Machine -> Machine] -> Machine -> Machine
+run :: Array PC (Machine -> Machine) -> Machine -> Machine
 run is m
-    | end pc = is !! pc $ m
-    | otherwise = run is (is !! pc $ m)
+    | end pc = is ! pc $ m
+    | otherwise = run is (is ! pc $ m)
         where pc = fromInteger . takePC $ m
-              end = (length is - 1 ==)
+              end = ((snd . bounds) is - 1 ==)
 
 prepare :: [Instruction] -> Machine -> Machine
 prepare is = appEndo . getDual $ foldMap (Dual . Endo . setLabel) is
+
+toArray :: [a] -> Array PC a
+toArray ls = listArray (0,toInteger $ length ls) ls
