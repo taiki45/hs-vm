11eef854c01ef515a16e6f754851247447430b0e
Taiki ONO <taiks.4559@gmail.com>
2014-04-06 17:28:37 +0900

    Add debugger

--- src/VM.hs
+++ src/VM.hs
@@ -2,7 +2,8 @@ module VM
     ( Instruction (..)
     , runVM
     , takeResult
-    , takePC ) where
+    , takePC
+    , testRunVM ) where
 
 import Control.Applicative ((<$>))
 import Data.Array
@@ -30,3 +31,29 @@ prepare is = appEndo . getDual $ foldMap (Dual . Endo . setLabel) is
 
 toArray :: [a] -> Array PC a
 toArray ls = listArray (0,toInteger $ length ls) ls
+
+testRunVM :: [Instruction] -> IO Machine
+testRunVM is = testRun (toArray $ instMorph <$> is) (return . setMain . setCounter 0 . prepare is $ initMachine)
+
+testRun :: Array PC (Machine -> Machine) -> IO Machine -> IO Machine
+testRun is m = do m' <- m
+                  let pc = fromInteger . takePC $ m'
+                      end = ((snd . bounds) is ==)
+                  input <- runDebugger pc m'
+                  if input == "exit" || end pc
+                    then m
+                    else testRun is (return (is ! pc $ m'))
+
+runDebugger :: PC -> Machine -> IO String
+runDebugger pc m = do putStrLn $ "[DEUBG] Now in " ++ show pc ++ " | input command"
+                      putStr "> "
+                      input <- getLine
+                      case input of
+                          "print m" -> print m >> runDebugger pc m
+                          "print ds" -> print (takeDS m) >> runDebugger pc m
+                          "print mem" -> print (takeMem m) >> runDebugger pc m
+                          "print cs" -> print (takeCS m) >> runDebugger pc m
+                          "print labels" -> print (takeL m) >> runDebugger pc m
+                          "next" -> return ""
+                          "exit" -> return "exit"
+                          _ -> runDebugger pc m
